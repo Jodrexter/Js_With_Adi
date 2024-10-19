166a180d83c4816c9df064c5013c286d298fd542
Adarsh Salgaonkar <143284129+Jodrexter@users.noreply.github.com>
2024-10-19 11:26:00 +0530

    Add files via upload

---
+++ 10_Functions/10_Functions.js
@@ -0,0 +1,38 @@
+const prompt=require("prompt-sync")(); // prompt is not working in vs code so you need to install npm install prompt-sync in terminal and write this query.
+
+// Functions >> a javascript functions is a block of code design to perform a particular task.
+
+// a = prompt("enter value of a");
+// b = prompt("enter value of b");
+ function jod(a,b){
+    console.log(a+b);
+    console.log(a-b);
+}
+ jod(4,6);
+/*
+function xx(x, y){
+    return (x + y);
+    console.log("done");
+}
+b = 5;
+c = 10;
+d = 11;
+e = 15;
+console.log("value of a is ",xx(b, c));
+
+console.log("value of b is ",xx(d, e));
+
+// another type of function using const .
+const ooo =(p, q)=>{
+    return (p + q);
+}
+
+console.log(ooo(8, 9));
+
+// take value but dont retrun anything 
+const huhu =()=>{
+    return("i am toh fine yaaar");
+}
+
+huhu();
+*/
