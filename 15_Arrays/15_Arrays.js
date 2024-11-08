a5f8204f0bc3565d0401ebbf2683b4e85516126c
Adarsh Salgaonkar <143284129+Jodrexter@users.noreply.github.com>
2024-11-08 20:46:49 +0530

    Add files via upload

---
+++ 15_Arrays/15_Arrays.js
@@ -0,0 +1,64 @@
+// array are the variables which can hold more than one value.
+
+// array's are mutable. 
+// array can be changed.
+// in js array are object when you check type of array it will show you object.
+
+let a = [1,2,3,4,"adi",false,null];
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+a[0] = 90; // updating a value.
+a[7] = 89; // adding a new value.
+//console.log(a[7]); // will be undefined bcoz array don't have 7 element .
+console.log("length of  arrya is :",a.length);
+console.log(a);
+console.log(typeof a);
+
+      
+let marks = [89,43,32,12,23,34];
+let n = marks.length;
+m  
+for(i=0;i<n;i++){
+    console.log(marks[i]);
+}
+
+
+let marks = [89,43,32,12,23,34];
+let i=0;
+for(i=0;i<marks.length;i++){
+    console.log(marks[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
